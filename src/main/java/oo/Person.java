1685b003a8883aa13d3ffc935be8b0259799510a
WONGTO6 <wongto6@oocl.com>
2024-11-21 13:38:07 +0800

    feat: complete step 1

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,18 @@
 package oo;
 
 public class Person {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
 }
