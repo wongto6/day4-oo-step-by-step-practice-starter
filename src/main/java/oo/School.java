b1828c4834818bdd586561fa2b89d63638bcec38
WONGTO6 <wongto6@oocl.com>
2024-11-21 21:29:00 +0800

    refactor: remove

--- src/main/java/oo/School.java
+++ src/main/java/oo/School.java
@@ -1,18 +1,17 @@
 package oo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class School {
 
     private List<Person> people;
 
-    public School(List<Person> people){
+    public School(List<Person> people) {
         this.people = people;
     }
 
-    public void startSchool(){
-        people.forEach(person-> System.out.println(person.introduce()));
+    public void startSchool() {
+        people.forEach(person -> System.out.println(person.introduce()));
     }
 
 }
